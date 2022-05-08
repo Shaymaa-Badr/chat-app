68e78446aeaf50ff0612dd4982ccd21c838342aa
Shaymaa-Badr <shaymaa.badr.g@gmail.com>
2022-05-08 15:14:18 +0200

    create UserModel

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,23 @@ import UserModel, { USER_TYPES } from "../models/UserModel.js";
 export default {
   onGetAllUsers: async (req, res) => {
     try {
+      const users = await UserModel.getUsers();
+      return res.status(200).json({ success: true, users });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error });
+    }
+  },
+  onGetUserById: async (req, res) => {
+    try {
+      const user = await UserModel.getUserById(req.params.id);
+      return res.status(200).json({ success: true, user });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error });
+    }
+    s;
+  },
+  onCreateUser: async (req, res) => {
+    try {
       const validation = makeValidation((types) => ({
         payload: req.body,
         checks: {
@@ -22,7 +39,15 @@ export default {
       return res.status(500).json({ success: false, error: error });
     }
   },
-  onGetUserById: async (req, res) => {},
-  onCreateUser: async (req, res) => {},
-  onDeleteUserById: async (req, res) => {},
+  onDeleteUserById: async (req, res) => {
+    try {
+      const user = await UserModel.deleteByUserById(req.params.id);
+      return res.status(200).json({
+        success: true,
+        message: `Deleted a count of ${user.deletedCount} user.`,
+      });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error });
+    }
+  },
 };
